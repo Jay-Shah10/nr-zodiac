932a149f3ea3939e832cb7aa9be86693ba20a363
Jay Shah <j24shah@gmail.com>
2020-06-26 23:38:14 -0400

    added more html pages.

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__) # name for the Flask app
 
-# defining a route. 
+# defining a route.
 @app.route("/", methods=['GET'])
 def home():
    return render_template('index.html')
@@ -10,60 +10,60 @@ def home():
 @app.route("/signs/")
 def showSigns():
     """
-    This method is will show all the signs that are there. 
+    This method is will show all the signs that are there.
     """
     return render_template('signs.html')
 
 @app.route("/signs/aries/")
 def aries():
-    return "hello aries"
+    return render_template('aries.html')
 
 @app.route("/signs/leo")
 def leo():
-    return "hello Leo"
+    return render_template('leo.html')
 
 @app.route("/signs/cancer")
 def cancer():
-    return "hello Cancer"
+    return render_template('cancer.html')
 
 @app.route("/signs/pisces")
 def pisces():
-    return 'Hello Pisces'
+    return render_template('pisces.html')
 
 @app.route("/signs/scorpio")
 def scorpio():
-    return "Hello Scorpio"
+    return render_template('scorpio.html')
 
 @app.route("/signs/taurus")
 def taurus():
-    return "Hello Taurus"
+    return render_template('taurus')
 
 @app.route("/signs/sagittarius")
 def sagittarius():
-    return "Hello Sagittaius"
+    return render_template('sagittarius.html')
 
 
 @app.route("/signs/gemini")
 def gemini():
-    return "Hello Gemini"
+    return render_template('gemini.html')
 
 @app.route("/signs/virgo")
 def virgo():
-    return "Hello Virgo"
+    return render_template('virgo.html')
 
 @app.route("/signs/libra")
 def libra():
-    return "Hello Libra"
+    return render_template('libra.html')
 
 @app.route("/signs/capricorn")
 def capricorn():
-    return "Hello Capricorn"
+    return render_template('capricorn.html')
 
 @app.route("/signs/aquarius")
 def aquarius():
-    return "Hello Aquarius"
+    return render_template('aquarius.html')
 
 if __name__ == "__main__":
-   # running the server. 
+   # running the server.
    app.run(debug = True)  # to allow for debugging and auto-reload
 
